5e1b604c0926d55d9743373af62a497c0cffbf4a
MahtabAmeli <mahtab.ameli@gmail.com>
2023-02-03 01:53:46 -0500

    Added comments to FloorEventBufferTest.

--- src/FloorEventBufferTest.java
+++ src/FloorEventBufferTest.java
@@ -1,15 +1,18 @@
 import java.io.IOException;
 
 /** *
- * Class FloorEventBufferTest tests communication between the floor thread (producer) and the scheduler thread (consumer).
+ * Class FloorEventBufferTest tests communication between the floor thread (producer)
+ * and the scheduler thread (consumer) by creating instances of each.
  *
  * @author Mahtab Ameli
  */
 class FloorEventBufferTest {
 
     public static void main(String[] args) throws IOException {
-        FloorEventBuffer buffer;
+        // floor thread (producer) and scheduler thread (consumer) represent corresponding subsystems in the elevator system
         Thread floor, scheduler;
+        // the buffer used to communicate data between floor and scheduler threads
+        FloorEventBuffer buffer;
 
         buffer = new FloorEventBuffer();
         scheduler = new TempSchedulerThread(buffer);
