edb12940a5c8473515c2eee34fffd763fb22b6e0
MahtabAmeli <mahtab.ameli@gmail.com>
2023-02-03 00:23:09 -0500

    Created FloorEventBuffer class.

---
+++ src/FloorEventBuffer.java
@@ -0,0 +1,54 @@
+/**
+ * Buffer for communicating floorEvents between FloorThread and SchedulerThread.
+ *
+ * @author Mahtab Ameli
+ */
+public class FloorEventBuffer {
+
+    private static FloorEvent floorEvent = null; // elevator request data input from floor
+    private boolean empty = true; // true if buffer is empty
+
+
+    /**
+     * Puts objects of FloorEvent in the buffer for communication between the floor and the scheduler.
+     */
+    public synchronized void putFloorEvent(FloorEvent floorEvent) {
+        while (!empty) {
+            try {
+                wait();
+            } catch (InterruptedException e) {
+                return;
+            }
+        }
+        if (Thread.currentThread().getName() == "FLOOR") {
+            System.out.println(Thread.currentThread().getName() + " putting floorEvent data in buffer:");
+            this.floorEvent = floorEvent;
+        } else {
+            System.out.println("FLOOR is not the current thread.");
+        }
+        empty = false;
+        System.out.println(floorEvent);
+        notifyAll();
+    }
+
+
+    /**
+     * Gets floorEvent data placed by the floor, from the buffer.
+     */
+    public synchronized Object getFloorEvent() {
+        while (empty || (floorEvent == null)) {
+            try {
+                wait();
+            } catch (InterruptedException e) {
+                return null;
+            }
+        }
+        System.out.println("\n" + Thread.currentThread().getName() + " getting data from floor buffer...\n");
+        empty = true;
+        System.out.println(Thread.currentThread().getName() + " received floor event data:\n" + floorEvent);
+        System.out.println("-----------------------------------------------------------------------------");
+        notifyAll();
+        return floorEvent;
+    }
+
+}
