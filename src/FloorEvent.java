665c976dc6c65d9cd0d639c6e1099673136fb066
MahtabAmeli <mahtab.ameli@gmail.com>
2023-02-02 20:01:23 -0500

    Created FloorEvent class (floor input data type).

---
+++ src/FloorEvent.java
@@ -0,0 +1,38 @@
+/**
+ * Class FloorEvent represents the floor input data for requesting an elevator.
+ *
+ * @author Mahtab Ameli
+ */
+public class FloorEvent {
+
+    public enum FloorButton {UP, DOWN}; // FloorButton variable represents the direction button pressed elevator from floor
+    private String time; // time of elevator request (hh:mm:ss.mmm)
+    private int floorNumber; // the floor number elevator request is made from
+    private FloorButton floorButton; // the up/down direction button
+    private int elevatorButton; // destination floor button pressed inside the elevator
+
+
+    /**
+     * Constructor for the class.
+     * @param time time of elevator request (hh:mm:ss.mmm)
+     * @param floorNumber the floor number elevator request is made from
+     * @param floorButton the up/down direction button
+     * @param elevatorButton destination floor button pressed inside the elevator
+     */
+    public FloorEvent(String time, int floorNumber, FloorButton floorButton, int elevatorButton) {
+        this.time = time;
+        this.floorNumber = floorNumber;
+        this.floorButton = floorButton;
+        this.elevatorButton = elevatorButton;
+    }
+
+
+    /**
+     * String representation of floorEvents.
+     * @return String
+     */
+    public String toString() {
+        return "[Time: " + time + ", Floor #: " + floorNumber +
+                ", Floor Button: " + floorButton + ", Elevator Button: " + elevatorButton + "]";
+    }
+}
