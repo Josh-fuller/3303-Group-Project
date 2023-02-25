1c6de2c641679e04754efa1c6cbdc91a500492f1
MahtabAmeli <mahtab.ameli@gmail.com>
2023-02-25 11:40:52 -0500

    Added class ElevatorState

--- src/ElevatorStateMachine/ElevatorState.java
+++ src/ElevatorStateMachine/ElevatorState.java
@@ -1,2 +1,16 @@
-public abstract class ElevatorState {
+package ElevatorStateMachine;
+
+public class ElevatorState {
+    protected ElevatorStateMachine context;
+    public ElevatorState(ElevatorStateMachine elevator) {
+        this.context = elevator;
+    }
+    public ElevatorState() {}
+    public void handleMovingDown() throws InterruptedException {}
+    public void handleMovingUp() throws InterruptedException {}
+    public void handleStopping(){}
+    public void handleOpeningDoor() {}
+    public void handleClosingDoor() {}
+    public void handleApproachingFloor() throws InterruptedException {}
+
 }
