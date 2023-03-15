bac70dcf0ef853022a3d68f7426b8e1fe5a47f11
Winaford <113386334+Winaford@users.noreply.github.com>
2023-03-15 16:54:48 -0400

    Update SchedulerThreadTest.java

--- src/Tests/SchedulerThreadTest.java
+++ src/Tests/SchedulerThreadTest.java
@@ -13,31 +13,29 @@ import java.util.Arrays;
 
 public class SchedulerThreadTest {
 
-    private ElevatorBuffer ePutBuffer = new ElevatorBuffer();
-    private ElevatorBuffer eTakeBuffer = new ElevatorBuffer();
-    private ElevatorBuffer fPutBuffer = new ElevatorBuffer();
-    private ElevatorBuffer fTakeBuffer = new ElevatorBuffer();
     private SchedulerThread scheduler = new SchedulerThread();
 
     FloorEvent floorTest1 = new FloorEvent("01:00:00.000", 1, FloorEvent.FloorButton.UP, 1, 1);
     FloorEvent floorTest2 = new FloorEvent("01:00:00.000", 2, FloorEvent.FloorButton.UP, 3, 1);
     FloorEvent floorTest3 = new FloorEvent("01:00:00.000", 2, FloorEvent.FloorButton.DOWN, 1, 1);
-
-
+    
 
 
     @Test
-    public void testFPutAndFTakeHaveTheSameValues() {
-        fPutBuffer.put(floorTest1);
-
-        FloorEvent actualEvent = fPutBuffer.take();
-        assertEquals(floorTest1, actualEvent);
+    public void testDestinationFloor(){
+        
     }
+
     @Test
-    public void testFloorEventToByte() throws IOException {
-        //System.out.println(Arrays.toString(scheduler.floorEventToByte(floorTest1)));
-
+    public void testStopRequestWIthNoRequests(){
+        
     }
+    
+     @Test
+    public void testStopRequestWithRequests(){
+        
+    }
+    
     @Test
     public void testByteToFloorEvent() throws IOException, ClassNotFoundException {
        // FloorThread.floorEventToByte(floorTest1);
