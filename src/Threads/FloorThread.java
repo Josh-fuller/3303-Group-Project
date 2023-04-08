3404f036aeb1bf18d71b9258284b346772a043bb
Winaford <justinwinford@cmail.carleton.ca>
2023-04-08 11:31:19 -0400

    Updated Floor Message format

--- src/Threads/FloorThread.java
+++ src/Threads/FloorThread.java
@@ -88,12 +88,9 @@ public class FloorThread extends Thread {
     public byte[] buildFloorByteMsg(FloorEvent floorEvent) {
 
         array[0] = (byte) 0;
-        array[1] = (byte) 1;
-        array[2] = (byte) 3;
+        array[1] = (byte) 2;
         array[3] = (byte) floorEvent.getFloorNumber();
-        array[4] = (byte) 3;
         array[5] = (byte) floorEvent.getElevatorButton();
-        array[6] = (byte) floorEvent.getElevatorNum();
 
         byte[] bMsg = array;
         return bMsg;
