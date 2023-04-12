d444fd802d7d4017d0ad972ec87d8013ccee3f68
Justin <justinwinford@cmail.carleton.ca>
2023-04-11 23:41:27 -0400

    Created 4 elevators

--- src/MainPackage/main.java
+++ src/MainPackage/main.java
@@ -2,9 +2,11 @@ package MainPackage;
 
 import Threads.ElevatorThread;
 import Threads.FloorThread;
+import Threads.GUIThread;
 import Threads.SchedulerThread;
 
 import java.io.IOException;
+import java.util.ArrayList;
 
 public class main {
 
@@ -12,17 +14,29 @@ public class main {
 
         //declare and initialise everything
 
-        Thread elevator1, floor, scheduler, elevator2;
+        Thread elevator1,elevator2, elevator3, elevator4, floor, scheduler;
+        ArrayList<ElevatorThread> elevators = new ArrayList<>();
+        ElevatorThread e1,e2,e3,e4;
 
+        e1 = new ElevatorThread(1011,1);
+        e2 = new ElevatorThread(1012,2);
+        e3 = new ElevatorThread(1013,3);
+        e4 = new ElevatorThread(1014,4);
 
         // Create the floor,scheduler and elevator threads,
         // passing each thread a reference to the
 
-        elevator1 = new Thread(new
-                ElevatorThread(1011),"Elevator 1");
+        elevator1 = new Thread(e1,"Elevator 1");
+        elevator2 = new Thread(e2,"Elevator 2");
+        elevator3 = new Thread(e3,"Elevator 3");
+        elevator4 = new Thread(e4,"Elevator 4");
         System.out.println("Elevator Created");
 
 
+        elevators.add(e1);
+        elevators.add(e2);
+        elevators.add(e3);
+        elevators.add(e4);
 
         floor = new Thread(new
                 FloorThread(), "Floor");
@@ -33,8 +47,13 @@ public class main {
         System.out.println("Scheduler Created");
 
 
+        GUIThread gui = new GUIThread(elevators);
+
+        gui.start();
         elevator1.start();
-        //elevator2.start();
+        elevator2.start();
+        elevator3.start();
+        elevator4.start();
         floor.start();
         scheduler.start();
 
